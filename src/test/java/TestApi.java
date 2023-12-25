877dbcbab2143902a25ce569c0e67b0dcd885c32
Santiago Mosquera Mapura <santiagomosquera2@gmail.com>
2023-12-25 18:20:01 -0500

    Calculate size of arrays

--- src/test/java/TestApi.java
+++ src/test/java/TestApi.java
@@ -8,8 +8,10 @@ import org.example.pojos.People;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.arrayWithSize;
 import static org.hamcrest.Matchers.equalTo;
 
 public class TestApi {
@@ -34,10 +36,10 @@ public class TestApi {
 
         given()
                 .when()
-                .get("https://swapi.dev/api/people/1")
+                .get("https://swapi.dev/api/people/2")
                 .then()
                 .statusCode(200)
-                .body("name",equalTo("Luke Skywalker"));
+                .body("films.size()",equalTo(6));
 
 
 
